e6568e338eb920124f54029a0626d915ecaeae28
joemardev <joemarb.dev@gmail.com>
2023-07-13 13:11:38 +0800

    Update Product Json

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -1,53 +1,54 @@
-import { useEffect , useState} from "react";
+import { useEffect, useState } from "react";
 import { GetProductCategories } from "../../utils/app-functions.utils";
 import Slider from "react-slick";
 
-import Config from './carousel.config.json';
+import Config from "./carousel.config.json";
 import "slick-carousel/slick/slick.css";
-import './category.styles.scss';
+import "./category.styles.scss";
 import { useNavigate } from "react-router-dom";
 
-
 const Category = () => {
-    
-    const navigate = useNavigate();
-    const [categories , setCategories] = useState([]);
+	const navigate = useNavigate();
+	const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        setCategories(GetProductCategories());
-    },[]);
+	useEffect(() => {
+		setCategories(GetProductCategories());
+	}, []);
 
-    const GoToMealPage = (category) => {
-        navigate(`/meal/${category}`);
-    }
+	const GoToMealPage = (category) => {
+		navigate(`/meal/${category}`);
+	};
 
-    return (
-        <div className="categories-section">
-            
-            <Slider {...Config} >
-                {categories.map((item,index) => {
-                    const {name} = item;
-                    const {thumbnail_small} = item.image;
+	return (
+		<div className="categories-section">
+			<Slider {...Config}>
+				{categories.map((item, index) => {
+					const { name } = item;
+					const { thumbnail_small = null } = item.image || {};
 
-                    return (
-                            <div className="category-wrapper" key={index} onClick={() => GoToMealPage(name)}>
-                                
-                                <div className="category-body">
-                                    <div>
-                                
-                                        <img className="category-image" src={thumbnail_small} alt={name}/>
-                                     
-                                        <span className="category-title">  {name}</span>
-                                    </div>
-                                </div>
-                              
-                            </div>
-                       
-                    );
-                })}
-            </Slider>
-        </div>
-    )
-}
+					if (thumbnail_small === null) return;
+					return (
+						<div
+							className="category-wrapper"
+							key={index}
+							onClick={() => GoToMealPage(name)}>
+							<div className="category-body">
+								<div>
+									<img
+										className="category-image"
+										src={thumbnail_small}
+										alt={name}
+									/>
+
+									<span className="category-title"> {name}</span>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</Slider>
+		</div>
+	);
+};
 
 export default Category;
